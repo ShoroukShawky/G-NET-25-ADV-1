86f832c725f0d28d2f1b088af3a9e119409bcaee
ShoroukShawky <324243558@sha.edu.eg>
2026-03-27 01:19:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_25_ADV_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
