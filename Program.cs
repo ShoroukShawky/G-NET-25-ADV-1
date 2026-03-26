5ff2819dd51a27948807ab3189f211d176c086ef
ShoroukShawky <324243558@sha.edu.eg>
2026-03-27 01:32:48 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@
             // A generic class uses type parameters that are replaced with actual types when you create an instance
             // reusability, type safety, no casting
             #endregion
-        }
+            #region Q2
+        //      class Container<T>
+        //{
+        //    public T item;
+        //    public void Add(T value) => item = value;
+        //    public T Get() => item;
+        //}
+        #endregion
     }
+    }
+   
 }
